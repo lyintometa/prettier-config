9ec02dffe9a1fad9784622f81445d58c73039f0c
Jakob Reitberger <jakob.reitberger@outlook.com>
2026-01-17 23:15:49 +0100

    Add tailwind class sorting with prettier-plugin-tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
 /**
  * @see https://prettier.io/docs/configuration
- * @type {import("prettier").Config & import("@trivago/prettier-plugin-sort-imports").PluginConfig}
+ * @type {import("prettier").Config
+ * & import("@trivago/prettier-plugin-sort-imports").PluginConfig
+ * & import("prettier-plugin-tailwindcss").PluginOptions}
  */
 const config = {
   experimentalTernaries: true,
@@ -12,6 +14,21 @@ const config = {
   plugins: [import.meta.resolve('@trivago/prettier-plugin-sort-imports')],
   importOrder: ['react', '<THIRD_PARTY_MODULES>', '^[./]'],
   importOrderSeparation: true,
+}
+
+if (doesModuleExist('tailwindcss')) {
+  config.plugins.push(import.meta.resolve('prettier-plugin-tailwindcss'))
+  config.tailwindFunctions = ['classNames']
+  config.tailwindStylesheet = './src/index.css'
+}
+
+function doesModuleExist(name) {
+  try {
+    import.meta.resolve(name)
+    return true
+  } catch {
+    return false
+  }
 }
 
 export default config
