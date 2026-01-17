5db091d04286219084d8461c097833177dfe6c86
Jakob Reitberger <jakob.reitberger@outlook.com>
2026-01-17 14:50:14 +0100

    Initialize project and add first config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  experimentalTernaries: true,
+  experimentalOperatorPosition: 'start',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+}
+
+export default config
